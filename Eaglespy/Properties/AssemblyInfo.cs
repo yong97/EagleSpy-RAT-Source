7b857b64557831cc05dc6e775ebb9beac05e0a9f
L3m0NShare <161139798+L3m0NShare@users.noreply.github.com>
2024-04-28 18:40:12 +0200

    Add files via upload

---
+++ Eaglespy/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyTitle("EagleSpy")]
+[assembly: AssemblyDescription("EagleSpy")]
+[assembly: AssemblyCompany("EagleSpy")]
+[assembly: AssemblyProduct("EagleSpy")]
+[assembly: AssemblyCopyright("EagleSpy ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("c3d63286-8b96-42f5-ad23-b86c42994112")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[module: RefSafetyRules(11)]
+[module: SuppressIldasm]
